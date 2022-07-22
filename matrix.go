d61c8ce97bd4c1bf405f972a3ef6c7bef6ff6f0e
TacticalCatto <74693042+TacticalCatto@users.noreply.github.com>
2022-07-23 07:30:38 +0800

    fix: Go elapsed time

--- matrix.go
+++ matrix.go
@@ -1,48 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 const n = 2048
 
 func main() {
-    a := make([][]float32, n)
-    for i := range a {
-        a[i] = make([]float32, n)
-    }
-    b := make([][]float32, n)
-    for i := range b {
-        b[i] = make([]float32, n)
-    }
+	a := make([][]float32, n)
+	for i := range a {
+		a[i] = make([]float32, n)
+	}
+	b := make([][]float32, n)
+	for i := range b {
+		b[i] = make([]float32, n)
+	}
+	c := make([][]float32, n)
+	for i := range c {
+		c[i] = make([]float32, n)
+	}
 
-    for i := 0; i < n; i++ {
-        rand.Seed(time.Now().UnixNano())
+	for i := 0; i < n; i++ {
+		rand.Seed(time.Now().UnixNano())
 
-        for j := 0; j < n; j++ {
-            a[i][j] = rand.Float32()
-            b[i][j] = rand.Float32()
-        }
-    }
+		for j := 0; j < n; j++ {
+			a[i][j] = rand.Float32()
+			b[i][j] = rand.Float32()
+		}
+	}
 
-    start := time.Now().UnixNano()
+	st := time.Now()
 
-    c := make([][]float32, n)
-    for i := range c {
-        c[i] = make([]float32, n)
-    }
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			for k := 0; k < n; k++ {
+				c[i][j] += a[i][k] * b[j][k]
+			}
+		}
+	}
 
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            for k := 0; k < n; k++ {
-                c[i][j] += a[i][k] * b[j][k]
-            }
-        }
-    }
-
-    end := time.Now().UnixNano()
-
-    fmt.Println((end - start) / 1000000000.0)
+	fmt.Println(time.Since(st).Seconds())
 }
