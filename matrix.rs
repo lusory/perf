e254fa6af0804201369aae9f38369b3a3bb5f369
Matous Kucera <mk@kcra.me>
2022-07-23 16:53:06 +0200

    refactor: benchmark script and fix rust test

--- matrix.rs
+++ matrix.rs
@@ -1,30 +1,67 @@
-use rand::{thread_rng, Rng};
-use std::time::Instant;
+use std::time::{SystemTime, Instant, UNIX_EPOCH};
 
-const n: i32 = 2048;
+const N: usize = 2048;
 
-fn main() {
-    let mut rng = thread_rng();
-    let mut a: Vec<Vec<f64>> = vec![vec![0.0; n]; n];
-    let mut b: Vec<Vec<f64>> = vec![vec![0.0; n]; n];
-    let mut c: Vec<Vec<f64>> = vec![vec![0.0; n]; n];
-    
-    for i in 0..n {
-        for j in 0..n {
-            a[i][j] = rng.gen::<f64>();
-            b[i][j] = rng.gen::<f64>();
+// minimal crate-free Xorshift 128 random number generator
+
+const KX: u32 = 123456789;
+const KY: u32 = 362436069;
+const KZ: u32 = 521288629;
+const KW: u32 = 88675123;
+
+pub struct Rand {
+    x: u32,
+    y: u32,
+    z: u32,
+    w: u32,
+}
+
+impl Rand {
+    pub fn new(seed: u32) -> Rand {
+        Rand {
+            x: KX ^ seed,
+            y: KY ^ seed,
+            z: KZ,
+            w: KW,
         }
     }
-    
+
+    pub fn rand(&mut self) -> u32 {
+        let t = self.x ^ self.x.wrapping_shl(11);
+        self.x = self.y;
+        self.y = self.z;
+        self.z = self.w;
+        self.w ^= self.w.wrapping_shr(19) ^ t ^ t.wrapping_shr(8);
+        return self.w;
+    }
+
+    pub fn rand_float(&mut self) -> f64 {
+        (self.rand() as f64) / (<u32>::max_value() as f64)
+    }
+}
+
+fn main() {
+    let mut rng = Rand::new(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs_f32() as u32);
+    let mut a: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+    let mut b: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+    let mut c: Vec<Vec<f64>> = vec![vec![0.0; N]; N];
+
+    for i in 0..N {
+        for j in 0..N {
+            a[i][j] = rng.rand_float();
+            b[i][j] = rng.rand_float();
+        }
+    }
+
     let nw = Instant::now();
-    
-    for i in 0..n {
-        for j in 0..n {
-            for k in 0..n {
+
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
                 c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
-    
+
     println!("{}", nw.elapsed().as_secs());
 }
--- perf_benchmark.py
+++ perf_benchmark.py
@@ -6,16 +6,17 @@ from os import remove
 
 # tuples of (benchmarked subject, compile command, run command, files to clean up)
 TARGETS = [
-	("Java", ["javac", "MatrixMultiplication.java"], ["java", "MatrixMultiplication"], ["MatrixMultiplication.class"]),
-	("C", ["gcc", "matrix.c", "-o", "matrix"], ["./matrix"], ["matrix"]),
-	("C[-O2]", ["gcc", "-O2", "matrix.c", "-o", "matrix_O2"], ["./matrix_O2"], ["matrix_O2"]),
-	("C[-O3]", ["gcc", "-O3", "matrix.c", "-o", "matrix_O3"], ["./matrix_O3"], ["matrix_O3"]),
-	("C++", ["g++", "matrix.cpp", "-o", "matrix_cpp"], ["./matrix_cpp"], ["matrix_cpp"]),
-	("C++[-O2]", ["g++", "-O2", "matrix.cpp", "-o", "matrix_cpp_O2"], ["./matrix_cpp_O2"], ["matrix_cpp_O2"]),
-	("C++[-O3]", ["g++", "-O3", "matrix.cpp", "-o", "matrix_cpp_O3"], ["./matrix_cpp_O3"], ["matrix_cpp_O3"]),
-	("Python", None, ["python3", "matrix.py"], None),
-	("Go", None, ["go", "run", "matrix.go"], None),
-	("Rust", ["rustc", "matrix.rs", "-o", "matrix_rust"], ["./matrix_rust"], ["matrix_rust"])
+    ("Java", ["javac", "MatrixMultiplication.java"], ["java", "MatrixMultiplication"], ["MatrixMultiplication.class"]),
+    ("C", ["gcc", "matrix.c", "-o", "matrix"], ["./matrix"], ["matrix"]),
+    ("C[-O2]", ["gcc", "-O2", "matrix.c", "-o", "matrix_O2"], ["./matrix_O2"], ["matrix_O2"]),
+    ("C[-O3]", ["gcc", "-O3", "matrix.c", "-o", "matrix_O3"], ["./matrix_O3"], ["matrix_O3"]),
+    ("C++", ["g++", "matrix.cpp", "-o", "matrix_cpp"], ["./matrix_cpp"], ["matrix_cpp"]),
+    ("C++[-O2]", ["g++", "-O2", "matrix.cpp", "-o", "matrix_cpp_O2"], ["./matrix_cpp_O2"], ["matrix_cpp_O2"]),
+    ("C++[-O3]", ["g++", "-O3", "matrix.cpp", "-o", "matrix_cpp_O3"], ["./matrix_cpp_O3"], ["matrix_cpp_O3"]),
+    ("Go", None, ["go", "run", "matrix.go"], None),
+    ("Rust", ["rustc", "matrix.rs", "-o", "matrix_rust"], ["./matrix_rust"], ["matrix_rust"]),
+    # python is the slowest, so keep it at the end
+    ("Python", None, ["python3", "matrix.py"], None)
 ]
 
 # number of iterations
@@ -23,59 +24,72 @@ ITERATIONS = 5
 
 init(autoreset=True)
 
+
 class NonZeroExitCodeException(Exception):
-	def __init__(self, message):
-		super().__init__(message)
+    def __init__(self, message):
+        super().__init__(message)
 
-def compile(target, iter_num: int) -> float:
-	print(f"{Fore.RED}Iteration {Style.BRIGHT}{iter_num}{Style.NORMAL}/{ITERATIONS}{Fore.RESET} --- {Fore.GREEN}Compiling subject {target[0]}.")
-	start = time()
 
-	process = Popen(target[1])
-	process.wait()
+class Benchmark:
+    def __init__(self, targets, iterations: int):
+        self.targets = targets
+        self.iterations = iterations
+        self.iter_num = 0
+        self.compilation_times = {}
+        self.running_times = {}
 
-	if process.returncode != 0:
-		raise NonZeroExitCodeException(" ".join(target[1]))
+    def benchmark(self):
+        for target in self.targets:
+            for iteration in range(self.iterations):
+                compilation_time = 0
+                if target[1] is not None:
+                    compilation_time = compile(target, iteration + 1)
+                running_time = run(target, iteration + 1)
 
-	end = time()
-	elapsed = end - start
-	print(f"{Fore.RED}Iteration {Style.BRIGHT}{iter_num}{Style.NORMAL}/{ITERATIONS}{Fore.RESET} --- {Fore.YELLOW}Compilation of subject {target[0]} finished in {Style.BRIGHT}{elapsed}{Style.NORMAL} seconds.")
-	return elapsed
+                self.compilation_times.setdefault(target[0], []).append(compilation_time)
+                self.running_times.setdefault(target[0], []).append(running_time)
 
-def run(target, iter_num: int) -> float:
-	print(f"{Fore.RED}Iteration {Style.BRIGHT}{iter_num}{Style.NORMAL}/{ITERATIONS}{Fore.RESET} --- {Fore.GREEN}Running subject {target[0]}.")
+                if target[3] is not None:
+                    for file in target[3]:
+                        remove(file)
 
-	process = Popen(target[2], stdout=PIPE, stderr=PIPE)
-	process.wait()
+    def compile(self, target) -> float:
+        print(f"{Fore.RED}Iteration {Style.BRIGHT}{self.iter_num}{Style.NORMAL}/{self.iterations}{Fore.RESET} --- {Fore.GREEN}Compiling subject {target[0]}.")
+        start = time()
 
-	if process.returncode != 0:
-		raise NonZeroExitCodeException(" ".join(target[1]))
+        process = Popen(target[1])
+        process.wait()
 
-	elapsed = float("\n".join([i.decode("utf-8") for i in process.stdout.readlines()]).strip())
-	print(f"{Fore.RED}Iteration {Style.BRIGHT}{iter_num}{Style.NORMAL}/{ITERATIONS}{Fore.RESET} --- {Fore.YELLOW}Running of subject {target[0]} finished in {Style.BRIGHT}{elapsed}{Style.NORMAL} seconds.")
-	return elapsed
+        if process.returncode != 0:
+            raise NonZeroExitCodeException(" ".join(target[1]))
+
+        end = time()
+        elapsed = end - start
+        print(f"{Fore.RED}Iteration {Style.BRIGHT}{self.iter_num}{Style.NORMAL}/{self.iterations}{Fore.RESET} --- {Fore.YELLOW}Compilation of subject {target[0]} finished in {Style.BRIGHT}{elapsed}{Style.NORMAL} seconds.")
+        return elapsed
+
+    def run(self, target) -> float:
+        print(f"{Fore.RED}Iteration {Style.BRIGHT}{self.iter_num}{Style.NORMAL}/{self.iterations}{Fore.RESET} --- {Fore.GREEN}Running subject {target[0]}.")
+
+        process = Popen(target[2], stdout=PIPE, stderr=PIPE)
+        process.wait()
+
+        if process.returncode != 0:
+            raise NonZeroExitCodeException(" ".join(target[1]))
+
+        elapsed = float("\n".join([i.decode("utf-8") for i in process.stdout.readlines()]).strip())
+        print(f"{Fore.RED}Iteration {Style.BRIGHT}{self.iter_num}{Style.NORMAL}/{self.iterations}{Fore.RESET} --- {Fore.YELLOW}Running of subject {target[0]} finished in {Style.BRIGHT}{elapsed}{Style.NORMAL} seconds.")
+        return elapsed
+
+    def print_frames(self):
+        c_df = DataFrame(self.compilation_times)
+        print("\n\n\n---     Compilation times     ---\n", c_df)
+        print("\n--- Compilation average times ---\n\n", c_df.mean())
+        r_df = DataFrame(self.running_times)
+        print("\n\n\n---       Running times       ---\n", r_df)
+        print("\n---   Running average times   ---\n\n", r_df.mean())
 
 if __name__ == "__main__":
-	compilation_times = {}
-	running_times = {}
-
-	for target in TARGETS:
-		for iteration in range(ITERATIONS):
-			compilation_time = 0
-			if target[1] is not None:
-				compilation_time = compile(target, iteration + 1)
-			running_time = run(target, iteration + 1)
-
-			compilation_times.setdefault(target[0], []).append(compilation_time)
-			running_times.setdefault(target[0], []).append(running_time)
-
-			if target[3] is not None:
-				for file in target[3]:
-					remove(file)
-
-	c_df = DataFrame(compilation_times)
-	print("\n\n\n---     Compilation times     ---\n", c_df)
-	print("\n--- Compilation average times ---\n\n", c_df.mean())
-	r_df = DataFrame(running_times)
-	print("\n\n\n---       Running times       ---\n", r_df)
-	print("\n---   Running average times   ---\n\n", r_df.mean())
+    benchmark = Benchmark(TARGETS, ITERATIONS)
+    benchmark.benchmark()
+    benchmark.print_frames()
